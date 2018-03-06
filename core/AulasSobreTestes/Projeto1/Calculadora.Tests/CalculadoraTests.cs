d1ff6702e1291a8e433f1368dddd515112501dbd
Jefferson Vital Pedrosa <jefferson.pedrosa@sam.fortaleza.ce.gov.br>
2018-03-06 16:34:06 -0300

    Modulo 1 do curso de teste da Gaucia

---
+++ core/AulasSobreTestes/Projeto1/Calculadora.Tests/CalculadoraTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Tests
+{
+    public class CalculadoraTests
+    {
+        [TestFixture]
+        public class CalculadoraSimplesTests
+        {
+            [Test]
+            public void DeveAdicionarDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Adicionar(5, 5);
+                Assert.That(resultado, Is.EqualTo(10));
+            }
+            [Test]
+            public void DeveMultiplicarDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Multiplicar(5 , 3);
+                Assert.That(resultado, Is.EqualTo(15));
+            }
+            [Test]
+            public void DeveDiminuirDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Dimunuir(30, 5);
+                Assert.That(resultado, Is.EqualTo(25));
+            }
+            [Test]
+            public void DeveDividirDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Dividir(25, 5);
+                Assert.That(resultado, Is.EqualTo(5));
+            }
+        }
+    }
+}
---
+++ core/AulasSobreTestes/Projeto1/Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class CalculadoraSimples 
+    {
+        /*
+         * Metodo para adicionar 2 numeros
+         */
+         public int Adicionar(int num1,int num2)
+        {
+            return num1+num2;
+        }
+        /*
+         * Metodo para multiplicar 2 numeros
+         */
+         public int Multiplicar(int num1, int num2)
+        {
+            //parar fins de demo (bug) => reutrn num1 * num2;
+            return num1 * num2;
+        }
+        /*
+        * Metodo para Subtrair 2 numeros
+        */
+        public int Dimunuir(int num1, int num2)
+        {
+            //parar fins de demo (bug) => reutrn num1 * num2;
+            return num1 - num2;
+        }
+        /*
+        * Metodo para Dividir 2 numeros
+        */
+        public int Dividir(int num1, int num2)
+        {
+            //parar fins de demo (bug) => reutrn num1 * num2;
+            return num1 / num2;
+        }
+    }
+}
