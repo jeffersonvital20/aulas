97a5ce4b035b0a1013f1c7652f8642bbbeecc4cc
Jefferson Vital Pedrosa <jefferson.pedrosa@sam.fortaleza.ce.gov.br>
2018-04-10 14:36:57 -0300

    estudando testes

--- core/AulasSobreTestes/Demo_asserts/Demo_asserts.Tests/Igualdade/JuntarNomes.Tests.cs
+++ core/AulasSobreTestes/Demo_asserts/Demo_asserts.Tests/Igualdade/JuntarNomes.Tests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Demo_asserts.Tests
 {
+    
     public class JuntarNomesTests
     {
 
--- core/AulasSobreTestes/Demo_asserts/Demo_asserts/JuntarNomes.cs
+++ core/AulasSobreTestes/Demo_asserts/Demo_asserts/JuntarNomes.cs
@@ -8,5 +8,6 @@ namespace Demo_asserts
 {
     public class JuntarNomes
     {
+
     }
 }
