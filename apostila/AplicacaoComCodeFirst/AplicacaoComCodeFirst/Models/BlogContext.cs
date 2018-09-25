0258015705496fdd49f4338ee964b9a181d893b4
Pedrosa <jefferson.pedrosa@landisgyr.com>
2018-09-25 16:11:39 -0300

    Adicionando exercicio de uma apostila de MVC

---
+++ apostila/AplicacaoComCodeFirst/AplicacaoComCodeFirst/Models/BlogContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AplicacaoComCodeFirst.Models
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("name=BlogContext")
+        {
+            Database.Connection.ConnectionString = @"data source=DESKTOP-ET9UPCQ\SQLEXPRESS;
+                                                    initial catalog=BlogBDLivro; Integrated Security=SSPI";
+        }
+        public DbSet<Posts> Posts { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+    }
+}
